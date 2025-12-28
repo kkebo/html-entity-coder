fff22527df46f0d2ef8fdf1e628c9c6f2bd5632f
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2025-12-28 10:38:55 +0900

    feat: implement `encodeHTML`

--- Sources/HTMLEntityCoder/encodeHTML.swift
+++ Sources/HTMLEntityCoder/encodeHTML.swift
@@ -1,4 +1,19 @@
 /// Replaces any characters that aren't printable ASCII characters and `&`, `<`, `>`, `"`, and `'` with HTML character references.
-public func endodeHTML(_ input: String) -> String {
-    fatalError("not implemented")
+public func encodeHTML(_ input: String) -> String {
+    var output = ""
+    output.reserveCapacity(input.utf8.count)
+    for c in input {
+        switch reversedNamedChars[c] {
+        case let name?: output.append("&" + name)
+        case nil:
+            switch c {
+            case "\u{20}"..."\u{7E}": output.append(c)
+            case _:
+                output += c.unicodeScalars.lazy
+                    .map { "&#x" + String($0.value, radix: 16, uppercase: true) + ";" }
+                    .joined()
+            }
+        }
+    }
+    return output
 }
