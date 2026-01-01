bda1adf3cc7e371640d86d59a0fdd46cda1308ae
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2026-01-02 02:42:00 +0900

    fix: prefer a key that has the ";" suffix in `dedup`

--- Sources/HTMLEntityCoder/namedChars.swift
+++ Sources/HTMLEntityCoder/namedChars.swift
@@ -1,5 +1,11 @@
 private func dedup<S: StringProtocol>(lhs: S, rhs: S) -> S {
-    if lhs.count < rhs.count {
+    let lhsHasSuffix = lhs.hasSuffix(";")
+    let rhsHasSuffix = rhs.hasSuffix(";")
+    return if !lhsHasSuffix && rhsHasSuffix {
+        rhs
+    } else if lhsHasSuffix && !rhsHasSuffix {
+        lhs
+    } else if lhs.count < rhs.count {
         lhs
     } else if lhs.count > rhs.count {
         rhs
