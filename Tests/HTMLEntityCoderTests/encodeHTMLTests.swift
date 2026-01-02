a06bec23a97f2178200d2803ad804f2c47ec44c5
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2026-01-02 19:01:26 +0900

    test: add more tests for `encodeHTML`

--- Tests/HTMLEntityCoderTests/encodeHTMLTests.swift
+++ Tests/HTMLEntityCoderTests/encodeHTMLTests.swift
@@ -12,3 +12,13 @@ func encodeAllNamedEntities() async {
         }
     }
 }
+
+@Test
+func otherNonASCIISymbolsAreRepresentedThroughHexadecimalEscapes() {
+    #expect(encodeHTML("foo\u{A9}bar\u{1D306}baz\u{2603}qux") == "foo&copy;bar&#x1D306;baz&#x2603;qux")
+}
+
+@Test
+func encodeToNamedHTMLEntities() {
+    #expect(encodeHTML("\u{E4}\u{F6}\u{FC}\u{C4}\u{D6}\u{DC}") == "&auml;&ouml;&uuml;&Auml;&Ouml;&Uuml;")
+}
